1ad8f56b3986196a99c6136a482c33d72016273a
lottpett <kosty@HOME.localdomain>
2026-09-20 16:57:51 +0500

    Добавление обработки сигнала и статистики

--- src/main.c
+++ src/main.c
@@ -13,24 +13,57 @@
 #include <netinet/udp.h>
 #include <netinet/ip_icmp.h>
 #include <net/if.h>
+#include <signal.h>
+#include <errno.h>
 
 #define BUFFER_SIZE 65536
 
-//Флаги настроек
-int flag_verbose = 0;     // подробный вывод (-v)
-int flag_only_tcp = 0;    // только TCP (-t)
-int flag_only_udp = 0;    // только UDP (-u)
-int flag_show_data = 0;   // печатать данные (-d)
+int flag_verbose = 0;         // подробный вывод (-v)
+int flag_only_tcp = 0;        // только TCP (-t)
+int flag_only_udp = 0;        // только UDP (-u)
+int flag_show_data = 0;       // печатать данные (-d)
 char interface_name[32] = ""; // имя интерфейса (-i)
 
-//Печать данных в виде шестнадцатеричных байт
+// Флаг работы программы
+volatile sig_atomic_t keep_running = 1;
+
+//Счетчики для статистики
+unsigned long total_packets = 0;
+unsigned long count_tcp = 0;
+unsigned long count_udp = 0;
+unsigned long count_icmp = 0;
+unsigned long count_other = 0;
+unsigned long total_bytes = 0;
+
+// Обработчик сигнала Ctrl+C
+void sigint_handler(int signum) {
+    (void)signum;
+    keep_running = 0; // Переключаем флаг, чтобы цикл while завершился
+}
+
+// Печать итоговой статистики перед выходом
+void print_stats(void) {
+    printf("\n\n=========================================\n");
+    printf("           ИТОГОВАЯ СТАТИСТИКА           \n");
+    printf("=========================================\n");
+    printf("[*] Всего пакетов получено: %lu\n", total_packets);
+    printf("    - TCP:                  %lu\n", count_tcp);
+    printf("    - UDP:                  %lu\n", count_udp);
+    printf("    - ICMP (Ping):          %lu\n", count_icmp);
+    printf("    - Другие:               %lu\n", count_other);
+    printf("[*] Общий объем данных:     %.2f КБ (%lu байт)\n", 
+           (double)total_bytes / 1024.0, total_bytes);
+    printf("=========================================\n");
+    printf("Сниффер успешно завершил работу.\n");
+}
+
+// Печать данных в виде шестнадцатеричных байт
 void print_payload_simple(const unsigned char *data, int size) {
     if (size <= 0) return;
 
     printf("\n  [Data %d bytes]:\n  ", size);
     for (int i = 0; i < size; i++) {
         printf("%02X ", data[i]);
-        //переносим строку для читаемости
         if ((i + 1) % 16 == 0) {
             printf("\n  ");
         }
@@ -48,14 +81,12 @@ void process_tcp(const unsigned char *buffer, int size) {
     printf(" | Protocol: TCP | Port: %u -> %u", 
            ntohs(tcp->source), ntohs(tcp->dest));
 
-    // Если включен подробный режим (-v), покажем базовые флаги
     if (flag_verbose) {
         printf(" [SYN:%d ACK:%d FIN:%d]", tcp->syn, tcp->ack, tcp->fin);
     }
 
-    // Если включен флаг данных (-d)
     if (flag_show_data) {
-        unsigned short tcphdrlen = tcp->doff * 4; // размер заголовка TCP
+        unsigned short tcphdrlen = tcp->doff * 4;
         const unsigned char *data = buffer + tcphdrlen;
         int data_size = size - tcphdrlen;
         print_payload_simple(data, data_size);
@@ -68,7 +99,6 @@ void process_udp(const unsigned char *buffer, int size) {
     printf(" | Protocol: UDP | Port: %u -> %u", 
            ntohs(udp->source), ntohs(udp->dest));
 
-    // Если включен флаг данных (-d)
     if (flag_show_data) {
         unsigned short udphdrlen = sizeof(struct udphdr);
         const unsigned char *data = buffer + udphdrlen;
@@ -89,19 +119,17 @@ void process_packet(const unsigned char *buffer, int size) {
 
     struct ethhdr *eth = (struct ethhdr *)buffer;
 
-    // Интересует только IPv4
     if (ntohs(eth->h_proto) != ETH_P_IP) {
         return; 
     }
 
     struct iphdr *iph = (struct iphdr *)(buffer + sizeof(struct ethhdr));
 
-    // Фильтр: игнорируем 127.x.x.x
+    // Фильтр: игнорируем loopback 127.x.x.x
     if ((ntohl(iph->saddr) >> 24) == 127 || (ntohl(iph->daddr) >> 24) == 127) {
         return;
     }
 
-    // Проверяем фильтры по протоколам, если пользователь их задал
     if (flag_only_tcp && iph->protocol != IPPROTO_TCP) {
         return;
     }
@@ -109,14 +137,15 @@ void process_packet(const unsigned char *buffer, int size) {
         return;
     }
 
-    // Печать MAC-адресов
+    total_packets++;
+    total_bytes += size;
+
     printf("[L2] MAC: ");
     print_mac(eth->h_source);
     printf(" -> ");
     print_mac(eth->h_dest);
     printf(" | ");
 
-    // Печать IP
     struct sockaddr_in source, dest;
     memset(&source, 0, sizeof(source));
     source.sin_addr.s_addr = iph->saddr;
@@ -126,7 +155,6 @@ void process_packet(const unsigned char *buffer, int size) {
 
     printf("IP: %-15s -> %-15s", inet_ntoa(source.sin_addr), inet_ntoa(dest.sin_addr));
 
-    // Если включен подробный режим (-v), выводим TTL
     if (flag_verbose) {
         printf(" (TTL: %d)", iph->ttl);
     }
@@ -137,26 +165,32 @@ void process_packet(const unsigned char *buffer, int size) {
 
     switch (iph->protocol) {
         case IPPROTO_TCP:
+            count_tcp++;
             process_tcp(transport_header, transport_size);
             break;
         case IPPROTO_UDP:
+            count_udp++;
             process_udp(transport_header, transport_size);
             break;
         case IPPROTO_ICMP:
+            count_icmp++;
             process_icmp(transport_header, transport_size);
             break;
         default:
+            count_other++;
             printf(" | Protocol: Other (%d)\n", iph->protocol);
             break;
     }
 }
 
 int main(int argc, char *argv[]) {
+    // Регистрируем наш обработчик сигнала Ctrl+C
+    signal(SIGINT, sigint_handler);
 
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-v") == 0) {
             flag_verbose = 1;
-
+            
         } else if (strcmp(argv[i], "-t") == 0) {
             flag_only_tcp = 1;
 
@@ -165,7 +199,7 @@ int main(int argc, char *argv[]) {
 
         } else if (strcmp(argv[i], "-d") == 0) {
             flag_show_data = 1;
-
+            
         } else if (strcmp(argv[i], "-i") == 0 && i + 1 < argc) {
             strncpy(interface_name, argv[i + 1], sizeof(interface_name) - 1);
             i++;
@@ -195,12 +229,10 @@ int main(int argc, char *argv[]) {
         }
     }
 
-
     printf("=========================================\n");
     printf("           СНИФФЕР ЗАПУЩЕН               \n");
     printf("=========================================\n");
     
-    // Вывод включенных флагов
     if (strlen(interface_name) > 0) {
         printf("[*] Интерфейс:       %s\n", interface_name);
     } else {
@@ -218,12 +250,17 @@ int main(int argc, char *argv[]) {
     printf("[*] Подробный режим: %s\n", flag_verbose ? "ВКЛЮЧЕН (-v)" : "ВЫКЛ");
     printf("[*] Вывод данных:    %s\n", flag_show_data ? "ВКЛЮЧЕН (-d)" : "ВЫКЛ");
     printf("=========================================\n");
-    printf("Ожидание пакетов(Ctrl+C для выхода)...\n\n");
+    printf("Ожидание пакетов (нажмите Ctrl+C для выхода)...\n\n");
 
     unsigned char buffer[BUFFER_SIZE];
-    while (1) {
+
+    while (keep_running) {
         ssize_t data_size = recvfrom(sock_raw, buffer, BUFFER_SIZE, 0, NULL, NULL);
         if (data_size < 0) {
+
+            if (errno == EINTR) {
+                break;
+            }
             perror("Recvfrom error");
             close(sock_raw);
             return 1;
@@ -231,6 +268,9 @@ int main(int argc, char *argv[]) {
 
         process_packet(buffer, data_size);
     }
+
+    // Печатаем статистику перед выходом
+    print_stats();
 
     close(sock_raw);
     return 0;
